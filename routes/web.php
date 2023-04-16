6b0febe81e740b307020f50ea0851e4afdaea28c
Abdullah-Omar-p <abdullahomarj1@gmail.com>
2023-04-16 22:35:31 +0200

    update home page

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */  // ....      
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+Route::get('/homepage', [App\Http\Controllers\homepages\mainhome::class, 'index']);
+
