7612745793561f275ef72cc7c8ab080f9cf4d53d
Abdullah-Omar-p <abdullahomarj1@gmail.com>
2023-07-01 23:19:29 +0300

    upload form of login and middleware check user

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */  // ....      
 
-Route::get('/homepage', function () {
-    return view('index');
+Route::get('/loginform' , function(){
+    return view('loginform');
 });
 
 Route::get('/home' , function (){ //here the route for filter produtcts 
