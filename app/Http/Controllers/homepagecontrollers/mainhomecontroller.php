6c589f28611ead0756fa492b61ba7452e09033c0
Abdullah-Omar-p <abdullahomarj1@gmail.com>
2023-07-23 10:39:18 +0300

    edit array to for loop

--- app/Http/Controllers/homepagecontrollers/mainhomecontroller.php
+++ app/Http/Controllers/homepagecontrollers/mainhomecontroller.php
@@ -11,51 +11,23 @@ class mainhomecontroller extends Controller
 {
     public function __construct() {
         // .. Best Seller Part
-        // all have the same "Logic"..
-        $this->best_books = DB::table('categories')->select('image','name','price')
-        ->where('parent_id',1)->orderBy('Buy', 'desc')->take(10)->get();
 
-        $this->best_kitchen = DB::table('categories')->select('image','name','price')
-        ->where('parent_id',2)->orderBy('Buy', 'desc')->take(10)->get();
+        $Best_Seller = array();
 
-        $this->best_sports = DB::table('categories')->select('image','name','price')
-        ->where('parent_id',3)->orderBy('Buy', 'desc')->take(10)->get();
+           for ($i = 1; $i <= 10; $i++) {
+                $category = DB::table('categories')
+                    ->select('image', 'name', 'price')
+                    ->where('parent_id', $i)
+                    ->orderBy('credit', 'desc')
+                    ->take(10)
+                    ->get();
 
-        $this->best_electronics= DB::table('categories')->select('image','name','price')
-        ->where('parent_id',4)->orderBy('credit', 'desc')->take(10)->get();
-
-        $this->best_computers = DB::table('categories')->select('image','name','price')
-        ->where('parent_id',5)->orderBy('credit', 'desc')->take(10)->get();
-
-        $this->best_supermarket = DB::table('categories')->select('image','name','price')
-        ->where('parent_id',6)->orderBy('credit', 'desc')->take(10)->get();
-
-        $this->best_fashion = DB::table('categories')->select('image','name','price')
-        ->where('parent_id',7)->orderBy('credit', 'desc')->take(10)->get();
-
-        $this->best_home = DB::table('categories')->select('image','name','price')
-        ->where('parent_id',8)->orderBy('credit', 'desc')->take(10)->get();
-
-        $this->best_phones = DB::table('categories')->select('image','name','price')
-        ->where('parent_id',9)->orderBy('credit', 'desc')->take(10)->get();
-
-        $this->best_makeup = DB::table('categories')->select('image','name','price')
-        ->where('parent_id',10)->orderBy('credit', 'desc')->take(10)->get();
+                $Best_Seller[$i] = $category;
+            }
 
     }
     public function getdata()
     {
-
-        return view('homepage', compact(
-        'best_books',
-        'best_kitchen',
-        'best_sports',
-        'best_electronics',
-        'best_computers',
-        'best_supermarket',
-        'best_fashion',
-        'best_home',
-        'best_phones',
-        'best_makeup'));
+        return view('homepage', compact('Best_Seller'));
     }
 }
