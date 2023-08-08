1fab51bc59e795f91b651b3c816b6cc97bb1f10f
Abdullah-Omar-p <abdullahomarj1@gmail.com>
2023-08-08 13:01:15 +0300

    ''

--- app/Http/Controllers/AuthControllers/LogoutController.php
+++ app/Http/Controllers/AuthControllers/LogoutController.php
@@ -9,10 +9,10 @@ use Tymon\JWTAuth\Facades\JWTAuth;
 class LogoutController extends Controller
 {
     public function logout(Request $request){
-        // Get the token from the request header
+        
         $token = $request->header('Authorization');
 
-        // Check if the token is valid
+        // .. Check If Token Valid Or No ..
         if (!preg_match('/^Bearer\s+(.{32})$/', $token)) {
             return response()->json([
                 'status' => 'error',
@@ -27,23 +27,20 @@ class LogoutController extends Controller
         }
         if ($token) {
             try {
-                // Invalidate the token
                 JWTAuth::invalidate($token);
 
-                // Return a success response
+                // .. Return a Success Response ..
                 return response()->json([
                     'status' => 'success',
                     'message' => 'User logged out successfully',
                 ]);
             } catch (JWTException $e) {
-                // Return an error response
                 return response()->json([
                     'status' => 'error',
                     'message' => 'Failed to logout, please try again',
                 ], 500);
             }
         } else {
-            // Return an error response
             return response()->json([
                 'status' => 'error',
                 'message' => 'No token provided',
