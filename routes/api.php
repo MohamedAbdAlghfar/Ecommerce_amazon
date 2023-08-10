50a11f9e9ed9e00ca00281fa82a62c12ac767038
Abdullah-Omar-p <abdullahomarj1@gmail.com>
2023-08-10 17:16:14 +0300

    .

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,7 @@ use App\Http\Controllers\AuthControllers\{SignUpController,LoginController,Logou
 use App\Http\Controllers\ClientSideControllers\UserAccount\{DeleteAccountController,EditAccountController,CreateStoreController};
 use App\Http\Controllers\ClientSideControllers\HomePage\{MainHomeController};
 use App\Http\Controllers\ClientSideControllers\ProductType\{FiltersController,GetCardsController};
-use App\Http\Controllers\ClientSideControllers\ProductDetails_Payment\{AddCartController,DelCartController,GetCartProducts};
+use App\Http\Controllers\ClientSideControllers\ProductDetails_Payment\{AddCartController,DelCartController,GetCartProducts,GetSuggestedProducts,GetProductDetails};
 use App\Http\Middleware\{Is_Owner,Is_Owner_Assistant,Is_Store_Admin,Is_Store_Owner,Is_User};
 
 /*
