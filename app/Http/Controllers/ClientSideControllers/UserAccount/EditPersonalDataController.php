ec9d573593adc576aa07320cc107198f3bd5a395
Abdullah-Omar-p <abdullahomarj1@gmail.com>
2023-08-16 16:33:34 +0300

    create and edit store

--- app/Http/Controllers/ClientSideControllers/UserAccount/EditPersonalDataController.php
+++ app/Http/Controllers/ClientSideControllers/UserAccount/EditPersonalDataController.php
@@ -12,9 +12,47 @@ class EditPersonalDataController extends Controller
         $this->middleware('auth:api');
     }
 
-    public function update(Request $request, $id)
+    public function update(Request $request)
     {
-        //
+        $user = auth()->user();
+
+        $userId = $user->id;
+
+        $rules = [
+            'f_name'  => 'required',
+            'l_name'  => 'required',
+            'email'   => 'required|email',
+            'address' => 'required',
+            'gender'  => 'required',
+            'phone'   => 'required',
+            'password'=> 'required|min:8|confirmed',
+        ];
+        $this->validate($request, $rules);
+
+        // .. updating .. 
+        $updateUser = User::find($user->id);
+        $updateUser->f_name  = $request->f_name;
+        $updateUser->l_name  = $request->l_name;
+        $updateUser->email   = $request->email;
+        $updateUser->address = $request->address;
+        $updateUser->gender  = $request->gender;
+        $updateUser->phone   = $request->phone;
+        $updateUser->password= $request->password;
+
+        $updateUser->save();  
+
+        if ($updateUser) {
+            return response()->json([
+                'status' => 'Success',
+                'message'=> 'User Updated Successfully',
+                'user'  => $updateUser,
+            ]);
+        }
+        return response()->json([
+            'status' => 'Failed',
+            'message'=> 'Error In Updating User Data ! .. Try Again Later',
+        ]);
+        
     }
 
 }
