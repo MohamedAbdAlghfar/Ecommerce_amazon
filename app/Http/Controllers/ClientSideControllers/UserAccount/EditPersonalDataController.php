b4bd9809d9a7762eda3a096bd6a4dcfad48b970a
Abdullah-Omar-p <abdullahomarj1@gmail.com>
2023-08-18 15:20:43 +0300

    add change to editpersonaldatacontroller by add get user's data for changing it

--- app/Http/Controllers/ClientSideControllers/UserAccount/EditPersonalDataController.php
+++ app/Http/Controllers/ClientSideControllers/UserAccount/EditPersonalDataController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\ClientSideControllers\UserAccount;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Http\Exceptions\HttpResponseException;
 
@@ -14,6 +15,24 @@ class EditPersonalDataController extends Controller
         $this->middleware('auth:api');
     }
 
+    public function getData(){
+        $user_ = auth()->user();
+        $user_id = $user_->id;
+
+        $userPersonalData = User::where('id',$user_id)->get();
+
+        if ($userPersonalData) {
+            return response()->json([
+                'status'=>'success',
+                'data'  =>$userPersonalData,
+            ]);
+        }
+        return response()->json([
+            'status'=>'failed',
+            'data'  =>'failed to get the data !. try Again Later ',
+        ]);
+    }
+
     public function update(Request $request)
     {
         $user = auth()->user();
@@ -21,14 +40,15 @@ class EditPersonalDataController extends Controller
         $userId = $user->id;
 
         $rules = [
-            'f_name'  => 'nullable',
-            'l_name'  => 'nullable',
-            'email'   => 'nullable|email|unique:users',
-            'age'     => 'nullable',
-            'address' => 'nullable',
-            'gender'  => 'nullable',
-            'phone'   => 'nullable',
-            'password'=> 'nullable|min:8|confirmed',
+            'f_name'  => 'required',
+            'l_name'  => 'required',
+            'email'   => 'required|email|unique:users',
+            'age'     => 'required',
+            'profile_image'  => 'required',
+            'address' => 'required',
+            'gender'  => 'required',
+            'phone'   => 'required',
+            'password'=> 'required|min:8|confirmed',
         ];
         $this->validate($request, $rules);
 
@@ -39,6 +59,7 @@ class EditPersonalDataController extends Controller
         $updateUser->l_name  = $request->l_name;
         $updateUser->email   = $request->email;
         $updateUser->age     = $request->age;
+        $updateUser->profile_image  = $request->profile_image;
         $updateUser->address = $request->address;
         $updateUser->gender  = $request->gender;
         $updateUser->phone   = $request->phone;
